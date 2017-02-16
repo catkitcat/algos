1aaa14470a60bc7adda20f33a03f8f3e98da15f4
catkitcat <catkitcat@users.noreply.github.com>
2017-02-16 15:43:37 -0400

    added changes

---
+++ sort.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <cstdlib>//for "exit()" on some systems
+#include <vector>
+#include <string>
+
+using namespace std;
+
+/**
+\fn linearSearch
+\brief Search data for the first occurrence of key
+\param [in] key The value being searched for
+\param [in] data The data set that will be searched
+\returns location of key if found or -1 if not found
+*/
+int linearSearch(auto data, auto key)//prototype
+{	
+	for (int i=0; i < data.size(); i++)
+	{
+		if(data[i]==key)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+int main()
+{
+  vector<string> inputs;
+  string search_key, input;
+  int result;
+
+   cout<<"Welcome to \"search it\". We first need some input data."<<endl;
+   cout<<"We'll assume the inputs do not have any spaces."<<endl<<endl;
+   cout<<"To end input type the #-character (followed by Enter)"<<endl<<endl;
+
+   cin>>input;
+ 
+    while(input != "#")//read an unknown number of inputs from keyboard
+    {
+       inputs.push_back(input);
+       cin>>input;
+    }
+
+   cout<<endl<<"["<<inputs.size()<<" values read from input source]"<<endl<<endl;
+
+  if(inputs.size() == 0)//no input
+  {
+      cout<<endl<<"No input received, quiting..."<<endl<<endl;
+       exit(1);//nothing to do but quit program
+  }
+ 
+   cout<<endl<<"To end input type the #-character (followed by Enter)"<<endl<<endl;
+  cout<<"Enter a value to search for: ";
+
+
+   cin>>search_key;
+ 
+    /*while(search_key != "#")//perform searches until sentinel entered
+    {
+        result = linearSearch(inputs,search_key);
+
+        cout<<"  '"<<search_key<<"' was ";
+
+        if (result == -1)
+          cout<<"not found";
+        else
+          cout<<"found at index "<<result;
+
+
+        cout<<endl<<endl<<"Enter a value to search for: ";
+        cin>>search_key; 
+    }*/
+    void bubbleSort(auto& Data) {
+		for (int k = 1; k < Data.size(); k++)
+			for (int i = 0; i < Data.size() -1 -k; i++)
+				if (Data[i] > Data[i +1]){
+					//pre-defined swap function
+					
+					swap(Data[i], Data[i + 1]); 
+				} //end if
+	} //end function
+
+   cout<<endl<<"Program \"search it\" is now finished."<<endl<<endl;
+
+    return 0;
+}
